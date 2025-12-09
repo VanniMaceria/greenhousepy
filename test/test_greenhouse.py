3bf68192c7e1fe2e9e20bd600a8e2189d2729d39
VanniMaceria <andreabucci123@gmail.com>
2025-12-09 14:02:52 +0100

    Red phase user story #1

--- test/test_greenhouse.py
+++ test/test_greenhouse.py
@@ -9,8 +9,10 @@ from src.greenhouse import Greenhouse, GreenhouseError
 
 class TestGreenhouse(TestCase):
 
-    @patch.object(GPIO, "input")
-    def test_something(self, mock_object: Mock):
-        # This is an example of test where I want to mock the GPIO.input() function
-        pass
+    @patch.object(Seesaw, "moisture_read")
+    def test_read_moisture_range_300_500(self, moisture_sensor: Mock):
+        greenhouse = Greenhouse()
+        moisture_sensor.return_value = 300
+        moisture_level = greenhouse.measure_soil_moisture()
+        self.assertEqual(moisture_level, 300)
 
